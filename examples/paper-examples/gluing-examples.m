7d6bf738f98e3ffc2875b08e30661916320d3bcf
Sam Schiavone <sam.schiavone@gmail.com>
2023-07-12 16:59:35 -0400

    working on gluing example

--- examples/paper-examples/gluing-examples.m
+++ examples/paper-examples/gluing-examples.m
@@ -1,3 +1,5 @@
+// make curves to glue using formulas in Howe--Leprevost--Poonen
+QQ := RationalsExtra(20);
 K<t,u> := RationalFunctionField(QQ,2);
 bt := (16*t^3+16*t^2+6*t+1)/(8*t^2-1)^2;
 ct := (16*t^3+16*t^2+6*t+1)/(2*t*(4*t+1)*(8*t^2-1));
@@ -42,9 +44,25 @@ EC, mp := EllipticCurve(C, C![T,1,1]);
 P := C![T,-1,1];
 Q := EvaluateByPowerSeries(mp, P);
 P2 := Points((2*Q) @@ mp)[1];
+/*
 ev := [T,Eltseq(P2)[1]];
 Evaluate(ev,1);
+*/
 hev := [Evaluate(el, [T,Eltseq(P2)[1]]) : el in Coefficients(h)];
-Hev := HyperellipticCurve(hev);
-Factorization(hev);
+R3<z> := PolynomialRing(K2);
+hev := R3!hev;
+assert &and[el[2] eq 1 : el in Factorization(hev)];
+//Hev := HyperellipticCurve(hev);
+//
+hev1 := [Evaluate(el, 1) : el in Coefficients(hev)];
+hev1 := S!hev1;
+H1 := HyperellipticCurve(hev1);
+
+hev2 := [Evaluate(el, 2) : el in Coefficients(hev)];
+hev2 := S!hev2;
+H2 := HyperellipticCurve(hev2);
+
+// now glue
 AttachSpec("~/github/gluing/magma/spec");
+AllArithmetic2GluingsCCFor22(H1, H2, BaseField(H1));
+
