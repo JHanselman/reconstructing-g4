86d2ec0f9f6295e9377257f679aaba8594e43fdb
Sam Schiavone <sschiavo@mit.edu>
2023-08-21 11:37:33 -0400

    code cleanup: moving functions out of examples and into magma folder

--- examples/paper-examples/QMexample.m
+++ examples/paper-examples/QMexample.m
@@ -1,65 +1,7 @@
 AttachSpec("../../magma/spec");
 SetDebugOnError(true);
-load "Galois.m";
-load "gluingfuncs.m";
-function TakaseQuotientSq(theta4, eta, k, l, m)
-g := #Eltseq(eta[1]) div 2;
-U := {1,3,5};
-Bm := { 1..2*g+1 }; L := [ bp : bp in (Bm diff { k, l, m }) ];
-V := { L[i]: i in [1..g-1] }; W := { L[i]: i in [g..2*(g-1)] };
-num1 := theta4[TCharToIndex(EtaValue(eta, U sdiff (V join { k, l })))];
-num2 := theta4[TCharToIndex(EtaValue(eta, U sdiff (W join { k, l })))];
-den1 := theta4[TCharToIndex( EtaValue(eta, U sdiff (V join { k, m })))];
-den2 := theta4[TCharToIndex(EtaValue(eta, U sdiff (W join { k, m })))];
-return (num1*num2)/(den1*den2);
-end function;
-
-
-
-function HyperellipticCurveFromTheta4(theta4)
-g:=2;
-L:=Parent(theta4[1]);
-eta:=EtaFunction0(2);
-//ros := [ Sqrt(TakaseQuotientSq(theta4, eta, 1, l, 2)) : l in [3..2*g+1] ];
-ros := [];
-for l in [3..2*g+1] do
-  bool, s := IsSquare(TakaseQuotientSq(theta4, eta, 1, l, 2));
-  assert bool;
-  Append(~ros,s);
-end for;
-U:={1,3,5};
-R<X>:=PolynomialRing(L);
-for loo in [0..7] do
-  bin:=Intseq(loo,2,3);
-  rostest:= [ros[water]*(-1)^bin[water]: water in [1..3]];
-  rootstest := [0,1] cat [-ro/(ro-2): ro in rostest] cat [-1];
-  if #Seqset(rootstest) ne #rootstest then
-    print "found roots with opposite signs";
-    continue;
-  end if; 
-  ftest:=X*(X-1)*&*[(X-L!ro): ro in rostest];
-  //print IgusaInvariantsEqual(IgusaInvariants(f), IgusaInvariants(ftest));
-  theta4test:= [L!0: i in [1..16]];
-  for i in [1..5] do
-    for j in [i+1..5] do
-      T := {i,j, 6};
-      Tcomp := {1..6} diff T;
-      S := T sdiff U;
-      sign := (-1)^#(S meet U);
-      cha := EtaValue(eta, Setseq(S));
-      theta4test[TCharToIndex(cha)] := sign * &*[ &*[(rootstest[nu]-rootstest[mu])^(-1): nu in T]: mu in Tcomp];
-    end for;
-  end for;
-  if &and[theta4test[i]*theta4[1] eq theta4[i]*theta4test[1]: i in [1..16]] then
-    return ftest, rostest;
-  end if;
-end for;
-
-end function;
-
-
-
-
+//load "Galois.m";
+//load "gluingfuncs.m";
 
 R<x>:=PolynomialRing(Rationals());
 //f := 24*x^5 + 36*x^4 - 4*x^3 - 12*x^2 + 1;
@@ -78,41 +20,45 @@ Ig:=R4!Ig;
 TSpace := &+[Evaluate(Derivative(Ig, i), Poi) * R4.i: i in [1..5]];
 P4:=ProjectiveSpace(R4);
 Sch:=Scheme(P4, [Ig, TSpace]);
+print "looking for points on Igusa quartic";
 points := PointSearch(Sch, 500);
 L := BaseRing(U0);
 correctV :=0;
 falseV := 0;
 bad_Vs := [];
+print "looping over points found";
 for loo in [1..500] do
-printf "curve index = %o\n", loo;
-Poi2:=Eltseq(points[loo]);
-	TSpace2 := &+[Evaluate(Derivative(Ig, i), Poi2) * R4.i: i in [1..5]];
-	if Evaluate(TSpace2, Poi) eq 0 then
-		continue;
-	end if;
-	theta41part := Eltseq(Vector(L, Poi2)*U0);
-	vecs, equfin, xi := IgusaCoordinates();
-    evs := [Evaluate(x, theta41part) : x in xi];
-    if #Seqset(evs) ne #evs then
-      print "Found multiple gluings!";
-      printf "point: %o\n", points[loo];
+  printf "curve index = %o\n", loo;
+  Poi2:=Eltseq(points[loo]);
+  TSpace2 := &+[Evaluate(Derivative(Ig, i), Poi2) * R4.i: i in [1..5]];
+  if Evaluate(TSpace2, Poi) eq 0 then
+    continue;
+  end if;
+  theta41part := Eltseq(Vector(L, Poi2)*U0);
+  vecs, equfin, xi := IgusaCoordinates();
+  evs := [Evaluate(x, theta41part) : x in xi];
+  if #Seqset(evs) ne #evs then
+    print "Found multiple gluings!";
+    printf "point: %o\n", points[loo];
+    continue;
+  end if;
+  theta41 := [&+[vec[i]*theta41part[i]: i in [1..5]]: vec in vecs];
+  if #[th: th in theta41| th eq 0] gt 6 then
       continue;
-    end if;
-	theta41 := [&+[vec[i]*theta41part[i]: i in [1..5]]: vec in vecs];
-	if #[th: th in theta41| th eq 0] gt 6 then
-		continue;
-	end if;
-	f2, ros := HyperellipticCurveFromTheta4(theta41);
-	pl:=InfinitePlaces(L)[1];
-	S<X,Y>:=PolynomialRing(L,3);
-	equ := Y^2- Evaluate(f2, X);
-    /*
-	Surf:= RiemannSurface(equ, pl: Precision:= 80);
-	Pi := BigPeriodMatrix(Surf);
-	EndJ2:= EndomorphismAlgebra(Pi);
-	print EndJ2, loo;
-	print "\n";
-    */
+  end if;
+  f2, ros := HyperellipticCurveFromTheta4(theta41);
+  pl:=InfinitePlaces(L)[1];
+  S<X,Y>:=PolynomialRing(L,3);
+  equ := Y^2- Evaluate(f2, X);
+
+
+  /*
+  Surf:= RiemannSurface(equ, pl: Precision:= 80);
+  Pi := BigPeriodMatrix(Surf);
+  EndJ2:= EndomorphismAlgebra(Pi);
+  print EndJ2, loo;
+  print "\n";
+  */
 //end for;
 
 R:= Parent(f2);      
